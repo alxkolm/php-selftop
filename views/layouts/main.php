ab25f117add86208e3140e8b556b0f2787bec502
Alexey Kolmakov <alxkolm@bk.ru>
2015-06-24 23:42:01 +0300

    fix links in layout

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -36,7 +36,7 @@ AppAsset::register($this);
                 'options' => ['class' => 'navbar-nav navbar-right'],
                 'items' => [
                     ['label' => 'Records', 'url' => ['record/index']],
-                    ['label' => 'Summary', 'url' => ['/summary']],
+                    ['label' => 'Summary', 'url' => ['/summary/index']],
                     Yii::$app->user->isGuest ?
                         ['label' => 'Login', 'url' => ['/site/login']] :
                         ['label' => 'Logout (' . Yii::$app->user->identity->username . ')',
