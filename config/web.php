49153babc235428d2ea3cff323355ee56a1cd0fc
Alexey Kolmakov <alxkolm@bk.ru>
2015-06-27 22:27:08 +0300

    Set app timezone

--- config/web.php
+++ config/web.php
@@ -7,6 +7,7 @@ $config = [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'defaultRoute' => 'summary/index',
+    'timeZone' => 'Europe/Moscow',
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
