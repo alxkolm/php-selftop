5f673abfa84463025f3a5c4efc00bd95405fe056
Alexey Kolmakov <alxkolm@bk.ru>
2015-07-09 20:57:14 +0300

    Command for generate data files

--- commands/SelftopController.php
+++ commands/SelftopController.php
@@ -32,4 +32,23 @@ class SelftopController extends Controller
             function ($a) { echo $a.PHP_EOL; }
         );
     }
+
+    /**
+     * Output window titles and process_id.
+     */
+    public function actionTitleProcess()
+    {
+        $titles = Window::find()
+            ->select(['title', 'process_id'])
+            ->orderBy('title')
+            ->createCommand()
+            ->queryAll();
+        array_walk(
+            array_filter(
+                $titles,
+                function ($a) { return !empty(trim($a['title'])); }
+            ),
+            function ($a) { echo $a['process_id']. ' ' .$a['title'].PHP_EOL; }
+        );
+    }
 }
