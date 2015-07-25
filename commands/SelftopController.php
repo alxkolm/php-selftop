f9f19a83d7a0475be762429de4722a50f37d55b8
Alexey Kolmakov <alxkolm@bk.ru>
2015-07-26 00:07:07 +0300

    Generate train set for libsvm

--- commands/SelftopController.php
+++ commands/SelftopController.php
@@ -9,6 +9,7 @@
 namespace app\commands;
 
 
+use app\models\Record;
 use app\models\Window;
 use yii\console\Controller;
 
@@ -69,5 +70,71 @@ class SelftopController extends Controller
             $titles,
             function ($a) { echo $a['id'] . ' ' . trim($a['title']) . PHP_EOL; }
         );
+    }
+    /**
+     * Output window.title and record.id columns
+     */
+    public function actionTitleRecord()
+    {
+        $titles = Record::find()
+            ->joinWith('window')
+            ->select(['title', '{{record}}.id'])
+            ->orderBy('{{record}}.id')
+            ->createCommand()
+            ->queryAll();
+        array_walk(
+            $titles,
+            function ($a) { echo $a['id'] . ' ' . trim($a['title']) . PHP_EOL; }
+        );
+    }
+
+    public function actionTrainData()
+    {
+        $trainFile = fopen('train.svm', 'w');
+        $testFile = fopen('test.svm', 'w');
+
+        while($line = fgets(STDIN)){
+            if ($line[0] == '#'){
+                continue;
+            }
+            $line = trim($line);
+            // take label
+            $posLabel = strpos($line, ' ');
+            $label = substr($line, 0, $posLabel);
+
+            // remove comment
+            $posComment = strpos($line, '#');
+
+            $rest = trim(substr($line, $posLabel + 1, $posComment - $posLabel - 1));
+            if ($rest){
+                $featureTokens = explode(',', $rest);
+
+                $features = [];
+                foreach ($featureTokens as $token) {
+                    list($dimension, $weight) = explode('::', $token);
+                    $features[$dimension] = (float)$weight;
+                }
+
+                // find record
+                $record = Record::findOne(['id' => $label]);
+                $out = $testFile;
+                $label = 0;
+                if ($record->tasks){
+                    $out = $trainFile;
+                    $label = $record->tasks[0]->id;
+                }
+                fwrite($out, $label. ' ');
+                $parts = [
+                    '1:'.$record->window_id,
+                    '2:'.$record->window->process_id,
+                ];
+                foreach ($features as $dim => $weight){
+                    $parts[] = $dim.':'.$weight;
+                }
+                fwrite($out, implode(' ', $parts));
+                fwrite($out, PHP_EOL);
+            }
+
+        }
     }
 }
