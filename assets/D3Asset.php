e60a32d3d94ef40b1f3bffdbed151c922ed9746e
Alexey Kolmakov <alxkolm@bk.ru>
2015-09-21 23:08:12 +0300

    hcl colors

--- assets/D3Asset.php
+++ assets/D3Asset.php
@@ -16,6 +16,7 @@ class D3Asset extends AssetBundle
     public $sourcePath = null;
     public $js = [
         'https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js',
-        '/js/colorbrewer.js'
+        '/js/colorbrewer.js',
+        '/js/hcl-colors.js',
     ];
 }
---
+++ web/js/hcl-colors.js
@@ -0,0 +1,5 @@
+var hclColors = {
+    rainbow00: [
+        '#fe8b76','#fe8895','#f08db3','#d498cb','#aca5da','#7eb0db','#51b9ce','#37bdb7','#48bf98','#6abd78','#8eb85c','#b1b04a','#d0a547','#ea9854','#fb8d6c'
+    ]
+}
