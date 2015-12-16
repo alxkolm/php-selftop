dbb0a71b519215f3ef45c95f4d1a0d1978a1e215
Alexey Kolmakov <alxkolm@bk.ru>
2015-12-16 21:09:18 +0300

    mcl cluster

--- components/TransitionClusterHelper.php
+++ components/TransitionClusterHelper.php
@@ -52,4 +52,60 @@ class TransitionClusterHelper
         }
         return [$clusterRaw, $winIdCluster];
     }
+    public static function clusterizeMatrixMcl($transitionMatrix, $windows)
+    {
+        $flattenTransitionMatrix = StatsHelper::flattenTransitionMatrix($transitionMatrix, $windows);
+        $path = \Yii::getAlias('@runtime/transition_cluster');
+        FileHelper::createDirectory($path);
+        /** @var string $filename Temp file */
+        $filename    = tempnam($path, 'data');
+        $filenameMci = tempnam($path, 'mci');
+        $filenameTab = tempnam($path, 'tab');
+        $filenameClusterNative = tempnam($path, 'clstrn');
+        $filenameCluster = tempnam($path, 'clstr');
+
+
+
+        // Write strings to file
+        $f = fopen($filename, 'w');
+        foreach ($flattenTransitionMatrix as $key => $value){
+            fwrite($f, $value['source'] . "\t" . $value['target'] . "\t" . $value['value'] . PHP_EOL);
+        }
+        // matrix diagonal cells for consistent output
+        foreach ($windows as $key => $value){
+            fwrite($f, $key . "\t" . $key . "\t" . '0' . PHP_EOL);
+        }
+
+        fclose($f);
+        chmod($filename, 0666);
+
+        // make clusters
+        // mcxload --stream-mirror -abc scikit/matrix-norm.csv -o data.mci -write-tab data.tab
+        $cmd = "mcxload --stream-mirror -abc {$filename} -o {$filenameMci} -write-tab {$filenameTab}"
+            ." && mcl {$filenameMci} -o {$filenameClusterNative}"
+            ." && mcxdump -icl {$filenameClusterNative} -tabr {$filenameTab} -o {$filenameCluster}";
+        $exitCode = 0;
+        $result = exec($cmd, $clusterRaw, $exitCode);
+        if ($exitCode != 0){
+//            return [];
+            throw new Exception('Can\'t run cluster command. ' . $result);
+        }
+        chmod($filename, 0666);
+        chmod($filenameMci, 0666);
+        chmod($filenameTab, 0666);
+        chmod($filenameClusterNative, 0666);
+        chmod($filenameCluster, 0666);
+
+
+        $winIdCluster = [];
+        foreach ($windows as $key => $window){
+            $winIdCluster[$window['id']] = $clusterRaw[$key];
+        }
+        unlink($filename);
+        unlink($filenameMci);
+        unlink($filenameTab);
+        unlink($filenameClusterNative);
+        unlink($filenameCluster);
+        return [$clusterRaw, $winIdCluster];
+    }
 }
